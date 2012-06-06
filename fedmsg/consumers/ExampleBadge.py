3b3e03010245789f7bdc2e5df9fefed56b233021
Ross Delinger <rossdylan@csh.rit.edu>
2012-06-06 09:50:13 -0400

    An example badge that awards the example_badge

---
+++ fedmsg/consumers/ExampleBadge.py
@@ -0,0 +1,21 @@
+import badge
+
+class ExampleBadgesConsumer(badge.FedoraBadgesConsumer):
+    topic = "org.fedoraproject.*"
+
+    def __init__(self, hub):
+        self.name = "examplebadge"
+        super(badge.FedoraBadgesConsumer, self).__init__(hub, self.name)
+
+    def consume(self, msg):
+        topic, body = msg.get('topic'), msg.get('body')
+
+        if body.startswith("example_thing"):
+            parts = body.split(" ")[1:]
+            email = parts[0]
+            badge_id = "example_badge"
+            self.award_badge(email, badge_id)
+
+
+
+
