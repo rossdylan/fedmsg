5d8e92cf84e7b2430c249bf32b2f37e598cee07b
Ross Delinger <rossdylan@csh.rit.edu>
2012-06-08 11:42:39 -0400

    Removed unused import and documented ExampleBadgeConsumer

--- fedmsg/consumers/ExampleBadge.py
+++ fedmsg/consumers/ExampleBadge.py
@@ -1,9 +1,15 @@
-import badges
 from badges import FedoraBadgesConsumer
 import logging
 log = logging.getLogger("moksha.hub")
 
 class ExampleBadgesConsumer(FedoraBadgesConsumer):
+    """
+    An Example subclass of FedoraBadgesConsumer. It sets the name and topic
+    and then parses messages coming off the fedmsg bus to award badges
+
+    :type hub: Moksha hub
+    :param hub: The Moksha hub we are receiving messages from
+    """
     topic = "org.fedoraproject.*"
 
     def __init__(self, hub):
@@ -11,6 +17,15 @@ class ExampleBadgesConsumer(FedoraBadgesConsumer):
         super(ExampleBadgesConsumer, self).__init__(hub, self.name)
 
     def consume(self, msg):
+        """
+        Consume a single message off the bus and award badges based on it
+        to award a badge call self.award_badge with the Person email, and the
+        ID of the badge you want to award
+
+        :type msg: dict
+        :param msg: the message coming off the fedmsg bus
+        """
+
         topic, body = msg.get('topic'), msg.get('body')
         if type(body) == type(""):
             return
