97bbcb9c32b3c3d6a47dceab6e5521bfbc6034c9
Ross Delinger <rossdylan@csh.rit.edu>
2012-06-08 10:59:12 -0400

    Documented the entire dbapi.py file

--- fedmsg/consumers/badges/dbapi.py
+++ fedmsg/consumers/badges/dbapi.py
@@ -9,14 +9,44 @@ from datetime import datetime
 
 
 class TahrirDatabase(object):
+    """
+    Class for talking to the Tahrir database
+    It handles adding information nessicary to issue open badges
+
+    :type dburi: str
+    :param dburi: the sqlalchemy database URI
+    """
     def __init__(self, dburi):
         self.session_maker = sessionmaker(bind=create_engine(dburi))
 
     def badge_exists(self, badge_id):
+        """
+        Check to see if tis badge already exists in the database
+
+        :type badge_id: str
+        :param badge_id: The ID of a Badge
+        """
+
         session = scoped_session(self.session_maker)
         return session.query(Badge).filter_by(id=badge_id).count() != 0
 
     def add_badge(self, name, image, desc, criteria, issuer_id):
+        """
+        Add a new badge to the database
+
+        :type name: str
+        :param name: Name of the Badge
+
+        :type image: str
+        :param image: URL of the image for this Badge
+
+        :type criteria: str
+        :param criteria: The criteria of this Badge
+
+        :type issuer_id: int
+        :param issuer_id: The ID of the issuer who issues this Badge
+        """
+
         session = scoped_session(self.session_maker)
         badge_id = name.lower()
 
@@ -32,14 +62,38 @@ class TahrirDatabase(object):
             session.commit()
 
     def person_exists(self, person_email):
+        """
+        Check if a Person with this email is stored in the database
+
+        :type person_email: str
+        :param person_email: An email address to search the database for
+        """
+
         session = scoped_session(self.session_maker)
         return session.query(Person).filter_by(email=person_email).count() != 0
 
-    def get_person(self,person_email):
+    def get_person(self, person_email):
+        """
+        Convience function to retrieve a person object from an email
+
+        :type person_email: str
+        :param person_email: The email address of a Person in the database
+        """
+
         session = scoped_session(self.session_maker)
         return session.query(Person).filter_by(email=person_email).one()
 
     def add_person(self, person_id, email):
+        """
+        Add a new Person to the database
+
+        :type person_id: int
+        :param person_id: This person's unique ID
+
+        :type person_email: str
+        :param person_email: This Person's email address
+        """
+
         session = scoped_session(self.session_maker)
         if not self.person_exists(email):
             new_person = Person(
@@ -50,10 +104,33 @@ class TahrirDatabase(object):
             session.commit()
 
     def issuer_exists(self, issuer_id):
+        """
+        Check to see if an issuer with this ID is in the database
+
+        :type issuer_id: int
+        :param issuer_id: The unique ID of this issuer
+        """
+
         session = scoped_session(self.session_maker)
         return session.query(Issuer).filter_by(id=issuer_id).count() != 0
 
     def add_issuer(self, origin, name, org, contact):
+        """
+        Add a new issuer to the Database
+
+        :type origin: str
+        :param origin: This issuers origin
+
+        :type name: str
+        :param name: Name of this issuer
+
+        :type org: str
+        :param org: The org of this issuer
+
+        :type contact: str
+        :param contact: The Contact email for this issuer
+        """
+
         session = scoped_session(self.session_maker)
         issuer_id = hash(origin + name)
         if not self.issuer_exists(issuer_id):
@@ -69,6 +146,19 @@ class TahrirDatabase(object):
         return issuer_id
 
     def add_assertion(self, badge_id, person_email, issued_on):
+        """
+        Add an assertion (award a badge) to the database
+
+        :type badge_id: str
+        :param badge_id: ID of the badge to be issued
+
+        :type person_email: str
+        :param person_email: Email of the Person to issue the badge to
+
+        :type issued_on: DateTime
+        :param issued_on: DateTime object holding the date the badge was issued on
+        """
+
         session = scoped_session(self.session_maker)
         if issued_on == None:
             issued_on = datetime.now()
